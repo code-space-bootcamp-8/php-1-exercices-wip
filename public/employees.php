1a573b9c43ebc6175d11ee034b8ee144052845e9
realvaz <test@example.com>
2021-06-08 11:36:44 +0200

    Add MySQL connection

--- public/employees.php
+++ public/employees.php
@@ -1,5 +1,22 @@
 <?php
 
+use function PHPSTORM_META\type;
+
+$dbUser = 'root';
+$dbPassword = 'root';
+$dbHost = 'localhost';
+$dbDatabase = 'employees';
+
+try {
+    $dbConnexion = new PDO("mysql:host=${dbHost};dbname=${dbDatabase}", $dbUser, $dbPassword);
+    $dbConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Error en la conexión a la base de datos: ' . $e->getMessage();
+}
+
+?>
+<?php
+
 $people = [
     ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'age' => 20, 'city' => 'Benalmádena'],
     ['name' => 'Mari Carmen', 'email' => 'carmen@correo.com', 'age' => 15, 'city' => 'Fuengirola'],
---
+++ public/resources/employees.sql
@@ -0,0 +1,28 @@
+-- Crear la base de datos.
+CREATE DATABASE php_exercices
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE php_exercices;
+
+-- Crear la tabla de empleados.
+CREATE TABLE IF NOT EXISTS `employees` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(128) NOT NULL,
+    `age` INT NOT NULL,
+    `city` VARCHAR(128),
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
+)  ENGINE=INNODB;
+
+-- Insertar datos de prueba.
+INSERT INTO `employees` (`name`, `email`, `age`, `city`)
+    VALUES ('Carlos', 'carlos@correo.com', 20, 'Benalmádena'),
+        ('Carmen', 'carmen@correo.com', 15, 'Fuengirola'),
+        ('Carmelo', 'carmelo@correo.com', 17, 'Torremolinos'),
+        ('Carolina', 'carolina@correo.com', 18, 'Málaga');
+
+--  Añadir columna para la contraseña
+ALTER TABLE `employees`
+    ADD COLUMN `password` VARCHAR(255) AFTER `email`;
