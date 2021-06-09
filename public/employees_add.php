925c431f1f095e504289b1dc979dccdd503d5115
realvaz <test@example.com>
2021-06-09 13:44:09 +0200

    Sintaxis alternativa para añadir parametros

--- public/employees_add.php
+++ public/employees_add.php
@@ -3,14 +3,20 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
 $query = 'INSERT INTO `employees` (`name`, `email`, `age`, `city`) VALUES (:nombre, :correo, :edad, :ciudad);';
+$params = [
+    ':nombre' => $_POST['name'],
+    ':correo' => $_POST['email'],
+    ':edad' => $_POST['age'],
+    ':ciudad' => $_POST['city'],
+];
 
 $stm = $dbConnexion->prepare($query);
 
-$stm->bindParam(':nombre', $_POST['name']);
-$stm->bindParam(':correo', $_POST['email']);
-$stm->bindParam(':edad', $_POST['age']);
-$stm->bindParam(':ciudad', $_POST['city']);
+// $stm->bindParam(':nombre', $_POST['name']);
+// $stm->bindParam(':correo', $_POST['email']);
+// $stm->bindParam(':edad', $_POST['age']);
+// $stm->bindParam(':ciudad', $_POST['city']);
 
-$stm->execute();
+$stm->execute($params);
 
 header('Location: /employees.php?message=' . urlencode('El usuario '.$_POST['email'].' se ha añadido correctamente.'));
