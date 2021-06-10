e3e1d4a71e4dee0c3cc3f67468179a3a2f744bdb
realvaz <test@example.com>
2021-06-10 10:31:55 +0200

    Rcuperar JSON del boton con los datos de la persona

--- public/assets/main.js
+++ public/assets/main.js
@@ -4,6 +4,12 @@ const deleteButtons = document.getElementsByClassName("employees-delete-button")
 
 Array.from(deleteButtons).forEach(element => {
     element.addEventListener('click', () =>{
-        console.log(element);
+        let person = JSON.parse(element.dataset.person);
+
+        if(window.confirm('¿De verdad quieres eliminar a '+person.name+'?')) {
+            console.log('borrar');
+        } else {
+            console.log('no borrar');
+        }
     });
 });
