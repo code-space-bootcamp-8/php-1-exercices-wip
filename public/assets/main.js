47472aabda245703c76c9d13409fc862fd15bd27
realvaz <test@example.com>
2021-06-10 11:07:47 +0200

    Manejar la solicitud DELETE desde el fichero employees_delete.php

--- public/assets/main.js
+++ public/assets/main.js
@@ -6,10 +6,15 @@ Array.from(deleteButtons).forEach(element => {
     element.addEventListener('click', () =>{
         let person = JSON.parse(element.dataset.person);
 
-        if(window.confirm('¿De verdad quieres eliminar a '+person.name+'?')) {
-            console.log('borrar');
-        } else {
-            console.log('no borrar');
+        if(window.confirm('¿De verdad quieres eliminar a ' + person.name + '?')) {
+            fetch('/employees_delete.php', {
+                method: 'DELETE',
+                body: element.dataset.person
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
         }
     });
 });
